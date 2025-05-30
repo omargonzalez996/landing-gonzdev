a227bad1dbf532f66e09765553ea63d789eaddb6
Omar Gonzalez <108283912+omargonzalez996@users.noreply.github.com>
2025-05-30 14:27:45 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="bg-amber-500 text-black w-dvw">HOLLA</div>
+    </>
+  );
+}
+
+export default App;
