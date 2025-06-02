773c3eaade60122882835c123dbe75d7d1558872
unknown <omar.gonz996@gmail.com>
2025-06-02 14:54:04 -0600

    asdasdasd

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import { useEffect, useState } from "react";
+
+import data from "./data/data.json";
+
 import Hero from "./components/Hero";
+import About from "./components/About";
+
 function App() {
+  const [language, setLanguage] = useState("es");
+  const [currentData, setCurrentData] = useState(data[language]);
+
+  useEffect(() => {
+    console.log("language change")
+  }, [language]);
+
   return (
     <>
-      <main className="flex flex-col w-dvw h-dvh bg-slate-950 justify-start items-center p-10">
-        <content className="flex flex-col w-full my-10">
-          <Hero />
+      <main className="flex flex-col w-dvw h-dvh justify-start items-center bg-slate-950">
+        <content className="flex flex-col w-full">
+          <Hero heroData={currentData.hero} />
+          <About aboutData={currentData.about} />
         </content>
       </main>
     </>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import Balatro from "./bits/Balatro";
 
-const Hero = () => {
+const Hero = ({ heroData }) => {
   return (
-    <div className="bg-slate-900 p-10 rounded-2xl w-full min-h-[600px] items-center">
-      <div className="text-6xl font-bold">GONZDEV</div>
-      <div>Where Tech meets Creativity</div>
+    <div className="hero min-h-screen relative bg-slate-950">
+      <Balatro
+        isRotate={true}
+        mouseInteraction={false}
+        pixelFilter={300}
+      />
+      {/* Gradient overlay for bottom fade */}
+      <div
+        className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-slate-950 to-transparent pointer-events-none"
+      ></div>
+      <div className="flex flex-col items-center gap-2">
+        <img
+          src="/PWLOGO.png"
+          alt="GONZDEV Logo"
+          className="w-48 md:w-56 mb-4 mx-auto"
+        />
+        <div className="text-8xl md:text-9xl font-bold bebas-neue-regular">{heroData.title}</div>
+        <p className="text-2xl md:text-4xl">{heroData.subtitle}</p>
+      </div>
     </div>
   );
 };
